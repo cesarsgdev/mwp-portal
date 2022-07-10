b7020d74ee311c4187e3cbf46b147dae1833a1bc
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-07-09 20:44:59 -0500

    Started basic server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: `This is the main route` });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
