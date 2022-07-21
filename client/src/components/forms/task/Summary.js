fc23b71c15d4d7b830daba10f8836b8ed753e983
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-07-20 23:56:42 -0500

    Started to work on summary page of New Task form

--- client/src/components/forms/task/Summary.js
+++ client/src/components/forms/task/Summary.js
@@ -1,25 +1,9 @@
 import { useContext } from "react";
 import { NewTaskContext } from "./context/NewTaskContext";
 
-const Summary = ({ formState, editFunction }) => {
+const Summary = () => {
   const context = useContext(NewTaskContext);
-  return (
-    <div className="animationWrapper">
-      <h1>Your task summary...</h1>
-      <span>{context.form.category}</span>
-      <span>{context.form.name}</span>
-      <span>{context.form.url}</span>
-      <span>{context.form.instructions}</span>
-      <button
-        page={1}
-        onClick={(e) => {
-          context.handleEdit(e);
-        }}
-      >
-        Back to animation
-      </button>
-    </div>
-  );
+  return <div className="animationWrapper"></div>;
 };
 
 export default Summary;
